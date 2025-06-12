e97402b2ffcb8a7c20358910c19bb27d193849f3
Isc740 <isaazcantillo@gmail.com>
2025-06-12 16:00:47 -0500

    add vim-rzip plugin

--- config/plugins/vim-rzip.nix
+++ config/plugins/vim-rzip.nix
@@ -2,12 +2,12 @@
 {
   extraPlugins = [
     (pkgs.vimUtils.buildVimPlugin {
-      name = "rzip-vim";
+      name = "vim-rzip";
       src = pkgs.fetchFromGitHub {
         owner = "lbrayner";
         repo = "vim-rzip";
         rev = "f65400fed27b27c7cff7ef8d428c4e5ff749bf28";
-        hash = "sha256-0000000000000000000000000000000000000000000=";
+        hash = "sha256-xy7rNqDVqlGapKClrP5BhfOORlMzHOQ8oIc8FdZT/AE=";
       };
     })
   ];
